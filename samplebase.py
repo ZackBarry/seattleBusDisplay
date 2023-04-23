e8820c51618bf7ecad42e3d254a45961dba0b11c
Zack Barry <zackbarry13@gmail.com>
2023-04-22 20:05:04 -0700

    hyphen

--- samplebase.py
+++ samplebase.py
@@ -23,7 +23,7 @@ class SampleBase(object):
         self.parser.add_argument("--led-scan-mode", action="store", help="Progressive or interlaced scan. 0 Progressive, 1 Interlaced (default)", default=1, choices=range(2), type=int)
         self.parser.add_argument("--led-pwm-lsb-nanoseconds", action="store", help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. Default: 130", default=130, type=int)
         self.parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
-        self.parser.add_argument("--led-limit-refresh_hz", action="store", help="Limits the refresh rate of the LED panel", default=0, type=int)
+        self.parser.add_argument("--led-limit-refresh-hz", action="store", help="Limits the refresh rate of the LED panel", default=0, type=int)
         self.parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 0..4. Default: 1", default=1, type=int)
         self.parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation")
         self.parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. Default: RGB", default="RGB", type=str)
